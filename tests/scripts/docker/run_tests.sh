c47a30ed8ba026e8e3bfb79b18cd60c5f462b6a0
Andrea Spacca <andrea.spacca@elastic.co>
2021-10-15 18:21:11 +0200

    fix tests/scripts/docker/run_tests.sh

--- tests/scripts/docker/run_tests.sh
+++ tests/scripts/docker/run_tests.sh
@@ -11,11 +11,12 @@ docker run \
   -e LOCAL_USER_ID=$UID \
   -e PIP_CACHE=${docker_pip_cache} \
   -e WITH_COVERAGE=true \
-  -e PYTEST_JUNIT="--junitxml=/app/tests/elastic-serverless-agent-junit.xml"
+  -e PYTEST_JUNIT="--junitxml=/app/tests/elastic-serverless-agent-junit.xml" \
   -v ${pip_cache}:$(dirname ${docker_pip_cache}) \
   -v "$(dirname $(pwd))":/app \
+  -w /app \
   --rm run_tests \
-	/bin/bash \
+  /bin/bash \
   -c "pip install --user -U pip
       pip install --user -r tests/requirements/requirements.txt --cache-dir ${docker_pip_cache}
       timeout 5m /bin/bash ./tests/scripts/run_tests.sh"
