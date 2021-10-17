a48bbc84527cc0b2b447d585965f7c3939ffeeeb
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-10-17 10:15:42 +0100

    Run coverage to xml and store in the CI

--- tests/scripts/run_tests.sh
+++ tests/scripts/run_tests.sh
@@ -5,3 +5,4 @@ set -ex
 # delete any __pycache__ folders to avoid hard-to-debug caching issues
 find . -name __pycache__ -type d -exec rm -r {} +
 py.test -v ${PYTEST_ARGS} "${PYTEST_JUNIT}" tests
+coverage xml
