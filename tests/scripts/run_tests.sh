5547ab593a2a54a2ac64f646e496ecaa2a4b983c
Andrea Spacca <andrea.spacca@gmail.com>
2021-10-19 15:58:46 +0200

    Update tests/scripts/run_tests.sh
    Co-authored-by: endorama <526307+endorama@users.noreply.github.com>

--- tests/scripts/run_tests.sh
+++ tests/scripts/run_tests.sh
@@ -5,4 +5,5 @@ set -ex
 # delete any __pycache__ folders to avoid hard-to-debug caching issues
 find . -name __pycache__ -type d -exec rm -r {} +
 py.test -v ${PYTEST_ARGS} "${PYTEST_JUNIT}" tests
+# Transform coverage to xml so Jenkins can parse and report it
 coverage xml
