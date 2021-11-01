5dc9142f59a910767c67f0f38b74c32bd05c6cab
Andrea Spacca <andrea.spacca@elastic.co>
2021-11-01 15:07:53 +0100

    localstack from python is not supported

--- tests/handlers/aws/test_handler.py
+++ tests/handlers/aws/test_handler.py
@@ -8,16 +8,15 @@ import os
 from copy import deepcopy
 from typing import Any, Union
 from unittest import TestCase
+from unittest.mock import patch
 
 import docker
-import mock
 from elasticsearch import Elasticsearch
-from localstack.services import infra
 from localstack.utils import testutil
 from localstack.utils.aws import aws_stack
 
-from handlers.aws import lambda_handler
-
+from handlers.aws import lambda_handler, sqs_trigger
+from storage import S3Storage
 
 class ContextMock:
     aws_request_id = "aws_request_id"
@@ -58,7 +57,7 @@ class TestLambdaHandler(TestCase):
     ) -> None:
         client = aws_stack.connect_to_service("s3")
 
-        client.create_bucket(Bucket=bucket_name)
+        client.create_bucket(Bucket=bucket_name, ACL="public-read-write")
         client.put_object(Bucket=bucket_name, Key=key_name, Body=content, ContentType=content_type)
 
     def setUp(self) -> None:
@@ -67,6 +66,22 @@ class TestLambdaHandler(TestCase):
             self._docket_network = docker_client.networks.get("run_tests")
         except Exception:
             self._docket_network = docker_client.networks.create("run_tests")
+
+        self._localstack_container = docker_client.containers.run(
+            "localstack/localstack",
+            detach=True,
+            environment=["SERVICES=s3,sqs"],
+            network="run_tests",
+            ports={"4566/tcp": None},
+        )
+
+        while len(self._localstack_container.ports) == 0:
+            self._localstack_container = docker_client.containers.get(self._localstack_container.id)
+
+        for log_entry in self._localstack_container.logs(stream=True, follow=True):
+            assert isinstance(log_entry, bytes)
+            if b"Execution of \"preload_services\" took" in log_entry:
+                break
 
         self._elastic_container = docker_client.containers.run(
             "docker.elastic.co/elasticsearch/elasticsearch:7.15.1",
@@ -79,8 +94,14 @@ class TestLambdaHandler(TestCase):
         while len(self._elastic_container.ports) == 0:
             self._elastic_container = docker_client.containers.get(self._elastic_container.id)
 
-        os.environ["LAMBDA_EXECUTOR"] = "local"
-        infra.start_infra(asynchronous=True)
+        for log_entry in self._elastic_container.logs(stream=True, follow=True):
+            assert isinstance(log_entry, bytes)
+            if b"Cluster health status changed from [YELLOW] to [GREEN]" in log_entry:
+                break
+
+        localstack_port: str = self._localstack_container.ports["4566/tcp"][0]["HostPort"]
+        os.environ["TEST_S3_URL"] = f"http://localhost:{localstack_port}"
+        os.environ["TEST_SQS_URL"] = f"http://localhost:{localstack_port}"
 
         self._source_queue_info = testutil.create_sqs_queue("source-queue")
         self._continuing_queue_info = testutil.create_sqs_queue("continuing-queue")
@@ -102,6 +123,7 @@ class TestLambdaHandler(TestCase):
                   dataset: "redis.log"
                   namespace: "default"
                 """
+
         self._upload_content_to_bucket(
             content=config_yaml, content_type="text/plain", bucket_name="config-bucket", key_name="config.yaml"
         )
@@ -111,6 +133,13 @@ class TestLambdaHandler(TestCase):
             + "79191:C 08 Jul 2021 13:25:02.610 # Redis version=6.2.4, bits=64, commit=00000000, "
             + "modified=0, pid=79191, just started"
         ).encode("UTF-8")
+
+        self._upload_content_to_bucket(
+            content=redis_log,
+            content_type="text/plain",
+            bucket_name="test-bucket",
+            key_name="redis.log",
+        )
 
         self._upload_content_to_bucket(
             content=gzip.compress(redis_log),
@@ -127,104 +156,110 @@ class TestLambdaHandler(TestCase):
         os.environ["SQS_CONTINUE_URL"] = self._continuing_queue_info["QueueUrl"]
 
     def tearDown(self) -> None:
-        infra.stop_infra()
+        self._elastic_container.stop()
+        self._elastic_container.remove()
+
+        self._localstack_container.stop()
+        self._localstack_container.remove()
+
         try:
             self._docket_network.remove()
         except Exception:
             pass
 
-        self._elastic_container.stop()
-        self._elastic_container.remove()
-
-    @mock.patch("storage.S3Storage._s3_client", aws_stack.connect_to_service("s3"))
-    @mock.patch("handlers.aws.sqs_trigger._get_sqs_client", lambda: aws_stack.connect_to_service("sqs"))
     def test_lambda_handler(self) -> None:
-        ctx = ContextMock()
-        event = {
-            "Records": [
-                {
-                    "messageId": "9b745861-1171-489c-9748-799ed2a3d9da",
-                    "body": json.dumps(
+        filename: str = "redis.log.gz"
+        self._perform_test_lambda_handler(filename=filename)
+
+    def _perform_test_lambda_handler(self, filename: str) -> None:
+        with patch.object(S3Storage, "_s3_client", aws_stack.connect_to_service("s3")):
+            with patch.object(sqs_trigger, "_get_sqs_client", lambda: aws_stack.connect_to_service("sqs")):
+                ctx = ContextMock()
+                event = {
+                    "Records": [
                         {
-                            "Records": [
+                            "messageId": "9b745861-1171-489c-9748-799ed2a3d9da",
+                            "body": json.dumps(
                                 {
-                                    "eventVersion": "2.1",
-                                    "eventSource": "aws:s3",
-                                    "awsRegion": "eu-central-1",
-                                    "eventTime": "2021-09-08T18:34:25.042Z",
-                                    "eventName": "ObjectCreated:Put",
-                                    "s3": {
-                                        "s3SchemaVersion": "1.0",
-                                        "configurationId": "test-bucket",
-                                        "bucket": {"name": "test-bucket", "arn": "arn:aws:s3:::test-bucket"},
-                                        "object": {
-                                            "key": "redis.log.gz",
-                                        },
-                                    },
+                                    "Records": [
+                                        {
+                                            "eventVersion": "2.1",
+                                            "eventSource": "aws:s3",
+                                            "awsRegion": "eu-central-1",
+                                            "eventTime": "2021-09-08T18:34:25.042Z",
+                                            "eventName": "ObjectCreated:Put",
+                                            "s3": {
+                                                "s3SchemaVersion": "1.0",
+                                                "configurationId": "test-bucket",
+                                                "bucket": {"name": "test-bucket", "arn": "arn:aws:s3:::test-bucket"},
+                                                "object": {
+                                                    "key": f"{filename}",
+                                                },
+                                            },
+                                        }
+                                    ]
                                 }
-                            ]
-                        }
-                    ),
-                    "eventSource": "aws:sqs",
-                    "eventSourceARN": self._source_queue_info["QueueArn"],
-                },
-            ]
-        }
+                            ),
+                            "eventSource": "aws:sqs",
+                            "eventSourceARN": self._source_queue_info["QueueArn"],
+                        },
+                    ]
+                }
 
-        first_call = lambda_handler(event, ctx)  # type:ignore
+                first_call = lambda_handler(event, ctx)  # type:ignore
 
-        assert first_call == "continuing"
+                assert first_call == "continuing"
 
-        while self._es_client.count(index="logs-redis.log-default")["count"] < 1:
-            pass
+                while self._es_client.count(index="logs-redis.log-default")["count"] < 1:
+                    pass
 
-        res = self._es_client.search(index="logs-redis.log-default")
-        assert res["hits"]["total"] == {"value": 1, "relation": "eq"}
-        assert (
-            res["hits"]["hits"][0]["_source"]["fields"]["message"]
-            == "79191:C 08 Jul 2021 13:25:02.609 # oO0OoO0OoO0Oo Redis is starting oO0OoO0OoO0Oo"
-        )
-        assert res["hits"]["hits"][0]["_source"]["fields"]["log"] == {
-            "offset": 0,
-            "file": {"path": "https://test-bucket.s3.eu-central-1.amazonaws.com/redis.log.gz"},
-        }
-        assert res["hits"]["hits"][0]["_source"]["fields"]["aws"] == {
-            "s3": {
-                "bucket": {"name": "test-bucket", "arn": "arn:aws:s3:::test-bucket"},
-                "object": {"key": "redis.log.gz"},
-            }
-        }
-        assert res["hits"]["hits"][0]["_source"]["fields"]["cloud"] == {"provider": "aws", "region": "eu-central-1"}
+                res = self._es_client.search(index="logs-redis.log-default")
+                assert res["hits"]["total"] == {"value": 1, "relation": "eq"}
+                assert (
+                    res["hits"]["hits"][0]["_source"]["fields"]["message"]
+                    == "79191:C 08 Jul 2021 13:25:02.609 # oO0OoO0OoO0Oo Redis is starting oO0OoO0OoO0Oo"
+                )
+                assert res["hits"]["hits"][0]["_source"]["fields"]["log"] == {
+                    "offset": 0,
+                    "file": {"path": f"https://test-bucket.s3.eu-central-1.amazonaws.com/{filename}"},
+                }
+                assert res["hits"]["hits"][0]["_source"]["fields"]["aws"] == {
+                    "s3": {
+                        "bucket": {"name": "test-bucket", "arn": "arn:aws:s3:::test-bucket"},
+                        "object": {"key": f"{filename}"},
+                    }
+                }
+                assert res["hits"]["hits"][0]["_source"]["fields"]["cloud"] == {"provider": "aws", "region": "eu-central-1"}
 
-        event = self._event_from_sqs_message()
-        second_call = lambda_handler(event, ctx)  # type:ignore
+                event = self._event_from_sqs_message()
+                second_call = lambda_handler(event, ctx)  # type:ignore
 
-        assert second_call == "continuing"
+                assert second_call == "continuing"
 
-        while self._es_client.count(index="logs-redis.log-default")["count"] < 2:
-            pass
+                while self._es_client.count(index="logs-redis.log-default")["count"] < 2:
+                    pass
 
-        res = self._es_client.search(index="logs-redis.log-default")
-        assert res["hits"]["total"] == {"value": 2, "relation": "eq"}
-        assert (
-            res["hits"]["hits"][1]["_source"]["fields"]["message"]
-            == "79191:C 08 Jul 2021 13:25:02.610 # Redis version=6.2.4, bits=64, commit=00000000, "
-            + "modified=0, pid=79191, just started"
-        )
+                res = self._es_client.search(index="logs-redis.log-default")
+                assert res["hits"]["total"] == {"value": 2, "relation": "eq"}
+                assert (
+                    res["hits"]["hits"][1]["_source"]["fields"]["message"]
+                    == "79191:C 08 Jul 2021 13:25:02.610 # Redis version=6.2.4, bits=64, commit=00000000, "
+                    + "modified=0, pid=79191, just started"
+                )
 
-        assert res["hits"]["hits"][1]["_source"]["fields"]["log"] == {
-            "offset": 81,
-            "file": {"path": "https://test-bucket.s3.eu-central-1.amazonaws.com/redis.log.gz"},
-        }
-        assert res["hits"]["hits"][1]["_source"]["fields"]["aws"] == {
-            "s3": {
-                "bucket": {"name": "test-bucket", "arn": "arn:aws:s3:::test-bucket"},
-                "object": {"key": "redis.log.gz"},
-            }
-        }
-        assert res["hits"]["hits"][1]["_source"]["fields"]["cloud"] == {"provider": "aws", "region": "eu-central-1"}
+                assert res["hits"]["hits"][1]["_source"]["fields"]["log"] == {
+                    "offset": 81,
+                    "file": {"path": f"https://test-bucket.s3.eu-central-1.amazonaws.com/{filename}"},
+                }
+                assert res["hits"]["hits"][1]["_source"]["fields"]["aws"] == {
+                    "s3": {
+                        "bucket": {"name": "test-bucket", "arn": "arn:aws:s3:::test-bucket"},
+                        "object": {"key": f"{filename}"},
+                    }
+                }
+                assert res["hits"]["hits"][1]["_source"]["fields"]["cloud"] == {"provider": "aws", "region": "eu-central-1"}
 
-        event = self._event_from_sqs_message()
-        third_call = lambda_handler(event, ctx)  # type:ignore
+                event = self._event_from_sqs_message()
+                third_call = lambda_handler(event, ctx)  # type:ignore
 
-        assert third_call == "completed"
+                assert third_call == "completed"
