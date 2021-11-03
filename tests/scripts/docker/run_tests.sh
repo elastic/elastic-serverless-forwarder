f651e46ba40d9af52553bfbe16119b06d26de2a3
Andrea Spacca <andrea.spacca@elastic.co>
2021-11-03 15:29:36 +0100

    add $HOME to $PATH

--- tests/scripts/docker/run_tests.sh
+++ tests/scripts/docker/run_tests.sh
@@ -22,4 +22,4 @@ docker run \
   -c "sudo service docker start
       pip install --user -U pip
       pip install --user -r tests/requirements/test-reqs.txt --cache-dir ${docker_pip_cache}
-      timeout 15m /bin/bash ./tests/scripts/run_tests.sh"
+      PATH=\${PATH}:\${HOME}/.local/bin/ timeout 15m /bin/bash ./tests/scripts/run_tests.sh"
--- tests/scripts/run_tests.sh
+++ tests/scripts/run_tests.sh
@@ -4,7 +4,7 @@ set -ex
 
 # delete any __pycache__ folders to avoid hard-to-debug caching issues
 find . -name __pycache__ -type d -exec rm -r {} +
-${HOME}/.local/bin/py.test -v ${PYTEST_ARGS} "${PYTEST_JUNIT}" tests
+py.test -v ${PYTEST_ARGS} "${PYTEST_JUNIT}" tests
 # Transform coverage to xml so Jenkins can parse and report it
-${HOME}/.local/bin/coverage xml
-${HOME}/.local/bin/coverage html
+coverage xml
+coverage html
