9300959926833b22b57be6cd1cf4f148b5667ec7
Andrea Spacca <andrea.spacca@elastic.co>
2021-11-24 16:54:22 +0100

    Dist script review (#36)
    * apply robots review outcoume to dist script
    
    * refactor message

--- .internal/aws/scripts/dist.sh
+++ .internal/aws/scripts/dist.sh
@@ -1,19 +1,36 @@
 #!/usr/bin/env bash
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
 
 set -e
 
-if [[ $# -ne 4 ]]
+echo "    AWS CLI (https://aws.amazon.com/cli/) and Python3.9 with pip3 required"
+echo "    Please, execute from root folder of the repo"
+
+if [[ $# -ne 5 ]]
 then
-    echo "Usage: $0 sar-app-name bucket-name account-id region"
+    echo "Usage: $0 sar-app-name semantic-version bucket-name account-id region"
+    echo "    Arguments:"
+    echo "    sar-app-name: name of the app to be deployed in SAR"
+    echo "    semantic-version: semantic version of the app to deploy in SAR"
+    echo "    bucket-name: bucket name where to store the zip artifact for SAR code"
+    echo "                 (it will be created if it doesn't exists, otherwise "
+    echo "                  you need already to have proper access to it)"
+    echo "    account-id: AWS account id to use for deploying"
+    echo "    region: region where to deploy in SAR for"
     exit 1
 fi
 
 SAR_APP_NAME="$1"
-BUCKET="$2"
-ACCOUNT_ID="$3"
-REGION="$4"
+SEMANTIC_VERSION="$2"
+BUCKET="$3"
+ACCOUNT_ID="$4"
+REGION="$5"
 
 TMPDIR=$(mktemp -d /tmp/dist.XXXXXXXXXX)
+
+trap "rm -rf ${TMPDIR}" EXIT
 
 mkdir "${TMPDIR}/packages"
 pip3 install --upgrade --target "${TMPDIR}/packages" -r requirements.txt
@@ -58,9 +75,8 @@ EOF
 
 aws s3api put-bucket-policy --bucket "${BUCKET}" --policy "file://${TMPDIR}/policy.json"
 
-sed -e "s/%sarAppName%/${SAR_APP_NAME}/g" -e "s/%codeURIBucket%/${BUCKET}/g" -e "s/%accountID%/${ACCOUNT_ID}/g" -e "s/%awsRegion%/${REGION}/g" .internal/aws/cloudformation/template.yaml > "${TMPDIR}/template.yaml"
+sed -e "s/%sarAppName%/${SAR_APP_NAME}/g" -e "s/%semanticVersion%/${SEMANTIC_VERSION}/g" -e "s/%codeURIBucket%/${BUCKET}/g" -e "s/%accountID%/${ACCOUNT_ID}/g" -e "s/%awsRegion%/${REGION}/g" .internal/aws/cloudformation/template.yaml > "${TMPDIR}/template.yaml"
 
 sam package --template-file "${TMPDIR}/template.yaml" --output-template-file "${TMPDIR}/packaged.yaml" --s3-bucket "${BUCKET}"
 sam publish --template "${TMPDIR}/packaged.yaml" --region "${REGION}"
 
-rm -rf "${TMPDIR}"
