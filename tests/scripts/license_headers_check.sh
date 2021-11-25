9165a3f819d4b6b25653459b085664337ba14b5c
Andrea Spacca <andrea.spacca@elastic.co>
2021-11-25 14:13:35 +0100

    add license to bash script (#37)

--- tests/scripts/license_headers_check.sh
+++ tests/scripts/license_headers_check.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
 
 if [[ $# -eq 0 ]]
 then
@@ -6,7 +9,7 @@ then
     exit 1
 fi
 
-FILES=$(find . -iname "*.py" -not -path "./venv/*")
+FILES=$(find . \( -iname "*.py" -or -iname "*.sh" \) -not -path "./venv/*")
 for FILE in $FILES
 do
     MISSING=$(grep --files-without-match "Licensed under the Elastic License 2.0" "$FILE")
@@ -16,14 +19,27 @@ do
         then
             echo fix "$FILE"
             TMPFILE=$(mktemp /tmp/license.XXXXXXXXXX)
-            cat <<EOF > "$TMPFILE"
+            if [[ "$FILE" == *".sh" && $(grep "#!/usr/bin/env bash" "$FILE") ]]
+            then
+                cat <<EOF > "$TMPFILE"
+#!/usr/bin/env bash
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
+EOF
+            tail -n +2 "$FILE" >> "$TMPFILE"
+            mv "$TMPFILE" "$FILE"
+            chmod 755 "$FILE"
+            else
+                cat <<EOF > "$TMPFILE"
 # Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 
 EOF
-            cat "$FILE" >> "$TMPFILE"
-            mv "$TMPFILE" "$FILE"
+                cat "$FILE" >> "$TMPFILE"
+                mv "$TMPFILE" "$FILE"
+            fi
         else
             echo "File with missing copyright header:"
             echo "$MISSING"
