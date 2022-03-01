995f72f37a4b08d7b46c04f46cf7d7ccbf3b42f4
Andrea Spacca <andrea.spacca@elastic.co>
2022-03-01 16:25:51 +0900

    Allow passing an author name when publishing on SAR

--- .internal/aws/scripts/dist.sh
+++ .internal/aws/scripts/dist.sh
@@ -8,7 +8,7 @@ set -e
 echo "    AWS CLI (https://aws.amazon.com/cli/) and Python3.9 with pip3 required"
 echo "    Please, execute from root folder of the repo"
 
-if [[ $# -ne 5 ]]
+if [[ $# -ne 5 && $# -ne 6 ]]
 then
     echo "Usage: $0 sar-app-name semantic-version bucket-name account-id region"
     echo "    Arguments:"
@@ -19,6 +19,8 @@ then
     echo "                  you need already to have proper access to it)"
     echo "    account-id: AWS account id to use for deploying"
     echo "    region: region where to deploy in SAR for"
+    echo "    sar-author-name: name of the author of the app to be deployed in SAR"
+    echo "                  (default to Elastic))"
     exit 1
 fi
 
@@ -27,7 +29,7 @@ SEMANTIC_VERSION="$2"
 BUCKET="$3"
 ACCOUNT_ID="$4"
 REGION="$5"
-
+SAR_AUTHOR_NAME="${6:-Elastic}"
 TMPDIR=$(mktemp -d /tmp/dist.XXXXXXXXXX)
 
 trap "rm -rf ${TMPDIR}" EXIT
@@ -75,7 +77,7 @@ EOF
 
 aws s3api put-bucket-policy --bucket "${BUCKET}" --policy "file://${TMPDIR}/policy.json"
 
-sed -e "s/%sarAppName%/${SAR_APP_NAME}/g" -e "s/%semanticVersion%/${SEMANTIC_VERSION}/g" -e "s/%codeURIBucket%/${BUCKET}/g" -e "s/%accountID%/${ACCOUNT_ID}/g" -e "s/%awsRegion%/${REGION}/g" .internal/aws/cloudformation/template.yaml > "${TMPDIR}/template.yaml"
+sed -e "s/%sarAppName%/${SAR_APP_NAME}/g" -e "s/%sarAuthorName%/${SAR_AUTHOR_NAME}/g" -e "s/%semanticVersion%/${SEMANTIC_VERSION}/g" -e "s/%codeURIBucket%/${BUCKET}/g" -e "s/%accountID%/${ACCOUNT_ID}/g" -e "s/%awsRegion%/${REGION}/g" .internal/aws/cloudformation/template.yaml > "${TMPDIR}/template.yaml"
 
 sam package --template-file "${TMPDIR}/template.yaml" --output-template-file "${TMPDIR}/packaged.yaml" --s3-bucket "${BUCKET}" --region "${REGION}"
 sam publish --template "${TMPDIR}/packaged.yaml" --region "${REGION}"
