15be9ebf60d6a8ba7e7e3adc1730cf6a57495959
Edoardo Tenani <edoardo.tenani@pm.me>
2023-01-10 16:10:49 +0100

    fix event id check

--- tests/handlers/aws/test_logstash_output.py
+++ tests/handlers/aws/test_logstash_output.py
@@ -274,7 +274,7 @@ class TestLambdaHandlerLogstashOutputSuccess(TestCase):
 
         assert msgs[1]["fields"]["aws"]["cloudwatch"]["log_group"] == group_name
         assert msgs[1]["fields"]["aws"]["cloudwatch"]["log_stream"] == stream_name
-        assert msgs[1]["fields"]["aws"]["cloudwatch"]["event_id"] == event_ids_cloudwatch_logs[1]
+        assert msgs[1]["fields"]["aws"]["cloudwatch"]["event_id"] == event_ids_cloudwatch_logs[0]
         assert msgs[1]["fields"]["cloud"]["provider"] == "aws"
         assert msgs[1]["fields"]["cloud"]["region"] == os.environ["AWS_DEFAULT_REGION"]
         assert msgs[1]["fields"]["cloud"]["account"]["id"] == "000000000000"
