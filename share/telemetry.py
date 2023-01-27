160c307224904943b0742f80783ba0cbb1374949
Maurizio Branca <maurizio.branca@gmail.com>
2023-01-27 16:38:17 +0100

    Check TELEMETRY_ENABLED using a helper function

--- share/telemetry.py
+++ share/telemetry.py
@@ -14,12 +14,25 @@ from typing import Any, Optional, Protocol, TypeVar
 
 from aws_lambda_typing import context as context_
 
+def strtobool(val: str) -> bool:
+    """Convert a string representation of truth to true (1) or false (0).
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return True
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return False
+    else:
+        raise ValueError("invalid truth value {!r}".format(val))
+
 
 def is_telemetry_enabled() -> bool:
     try:
-        # @TODO fix this! bool("false") -> True
-        return bool(os.environ["TELEMETRY_ENABLED"])
-    except KeyError:  # TELEMETRY_ENABLED not in env dict
+        return strtobool(os.environ["TELEMETRY_ENABLED"])
+    except (KeyError, ValueError):  # TELEMETRY_ENABLED not in env dict
         return False
 
 
---
+++ tests/share/test_telemetry.py
@@ -0,0 +1,57 @@
+from unittest import TestCase
+
+import pytest
+
+from share.telemetry import is_telemetry_enabled
+
+
+@pytest.mark.unit
+def test_is_telemetry_enabled_with_true_values(monkeypatch):
+    monkeypatch.setenv("TELEMETRY_ENABLED", "true")
+    assert is_telemetry_enabled() is True
+
+    monkeypatch.setenv("TELEMETRY_ENABLED", "True")
+    assert is_telemetry_enabled() is True
+
+    monkeypatch.setenv("TELEMETRY_ENABLED", "1")
+    assert is_telemetry_enabled() is True
+
+    monkeypatch.setenv("TELEMETRY_ENABLED", "yes")
+    assert is_telemetry_enabled() is True
+
+    monkeypatch.setenv("TELEMETRY_ENABLED", "on")
+    assert is_telemetry_enabled() is True
+
+
+@pytest.mark.unit
+def test_is_telemetry_enabled_with_false_values(monkeypatch):
+    monkeypatch.setenv("TELEMETRY_ENABLED", "false")
+    assert is_telemetry_enabled() is False
+
+    monkeypatch.setenv("TELEMETRY_ENABLED", "False")
+    assert is_telemetry_enabled() is False
+
+    monkeypatch.setenv("TELEMETRY_ENABLED", "0")
+    assert is_telemetry_enabled() is False
+
+    monkeypatch.setenv("TELEMETRY_ENABLED", "no")
+    assert is_telemetry_enabled() is False
+
+    monkeypatch.setenv("TELEMETRY_ENABLED", "off")
+    assert is_telemetry_enabled() is False
+
+
+@pytest.mark.unit
+def test_is_telemetry_enabled_with_no_env(monkeypatch):
+    # in there is not `TELEMETRY_ENABLED` env variable,
+    # the telemetry should be disabled
+    monkeypatch.delenv("TELEMETRY_ENABLED")
+    assert is_telemetry_enabled() is False
+
+
+@pytest.mark.unit
+def test_is_telemetry_enabled_with_invalid_value(monkeypatch):
+    # in there is not `TELEMETRY_ENABLED` env variable,
+    # the telemetry should be disabled
+    monkeypatch.setenv("TELEMETRY_ENABLED", "hey")
+    assert is_telemetry_enabled() is False
