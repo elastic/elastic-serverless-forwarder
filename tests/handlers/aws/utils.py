ff1ae84545aa41cf329ecb74729423e396548fe4
Andrea Spacca <andrea.spacca@elastic.co>
2023-02-27 10:22:03 +0900

    new integration tests, single containers run, add es testcontainer, refactor logstash testcontainer

--- tests/handlers/aws/utils.py
+++ tests/handlers/aws/utils.py
@@ -15,12 +15,30 @@ import gzip
 import os.path
 import random
 import string
+import time
 from copy import deepcopy
-from typing import Any, Text
+from typing import Any, Union
 
 from botocore.client import BaseClient as BotoBaseClient
 
 from share import json_dumper
+
+_AWS_REGION = "us-east-1"
+
+_REMAINING_TIME_FORCE_CONTINUE_0ms = 0
+
+_S3_NOTIFICATION_EVENT_TIME: str = "2021-09-08T18:34:25.042Z"
+
+
+class ContextMock:
+    def __init__(self, remaining_time_in_millis: int = _REMAINING_TIME_FORCE_CONTINUE_0ms):
+        self._remaining_time_in_millis = remaining_time_in_millis
+
+    aws_request_id = "aws_request_id"
+    invoked_function_arn = "arn:aws:lambda:us-east-1:123456789:function:elastic-serverless-forwarder"
+
+    def get_remaining_time_in_millis(self) -> int:
+        return self._remaining_time_in_millis
 
 
 def _load_file_fixture(name: str) -> str:
@@ -36,18 +54,19 @@ def _load_file_fixture(name: str) -> str:
     return res
 
 
-def _class_based_id(klass: object, prefix: str = "", suffix: str = "") -> str:
+def _time_based_id(prefix: str = "", suffix: str = "") -> str:
+    now = int(datetime.datetime.utcnow().strftime("%s")) * 1000
     if prefix:
         prefix = f"{prefix}-"
     if suffix:
         suffix = f"-{suffix}"
 
-    return f"{prefix}{type(klass).__name__}{suffix}"
+    return f"{prefix}{now}{suffix}".lower()
 
 
 def _logs_create_cloudwatch_logs_group(client: BotoBaseClient, group_name: str) -> Any:
     client.create_log_group(logGroupName=group_name)
-    return client.describe_log_groups(logGroupNamePrefix=group_name)
+    return client.describe_log_groups(logGroupNamePrefix=group_name)["logGroups"][0]
 
 
 def _logs_create_cloudwatch_logs_stream(client: BotoBaseClient, group_name: str, stream_name: str) -> Any:
@@ -71,8 +90,9 @@ def _logs_upload_event_to_cloudwatch_logs(
 
 def _logs_retrieve_event_from_cloudwatch_logs(
     client: BotoBaseClient, group_name: str, stream_name: str
-) -> tuple[dict[str, Any], list[str]]:
+) -> tuple[dict[str, Any], list[str], list[int]]:
     collected_log_event_ids: list[str] = []
+    collected_log_event_timestamp: list[int] = []
     collected_log_events: list[dict[str, Any]] = []
 
     events = client.get_log_events(logGroupName=group_name, logStreamName=stream_name)
@@ -88,6 +108,7 @@ def _logs_retrieve_event_from_cloudwatch_logs(
 
         collected_log_events.append(log_event)
         collected_log_event_ids.append(event_id)
+        collected_log_event_timestamp.append(int(float(event["timestamp"])))
 
     data_json = json_dumper(
         {
@@ -100,72 +121,207 @@ def _logs_retrieve_event_from_cloudwatch_logs(
         }
     )
 
-    data_gzip = gzip.compress(data_json.encode("UTF-8"))
+    data_gzip = gzip.compress(data_json.encode("utf-8"))
     data_base64encoded = base64.b64encode(data_gzip)
 
-    return {"awslogs": {"data": data_base64encoded}}, collected_log_event_ids
+    return {"awslogs": {"data": data_base64encoded}}, collected_log_event_ids, collected_log_event_timestamp
 
 
 def _s3_upload_content_to_bucket(
-    client: BotoBaseClient, content: Text, key: str, bucket_name: str, content_type: str, acl: str = "public-read-write"
+    client: BotoBaseClient,
+    content: bytes,
+    key: str,
+    bucket_name: str,
+    content_type: str,
+    create_bucket: bool = True,
+    acl: str = "public-read-write",
 ) -> None:
-    client.create_bucket(Bucket=bucket_name, ACL=acl)
-    client.put_object(Bucket=bucket_name, Key=key, Body=content, ContentType=content_type)
+    if create_bucket:
+        client.create_bucket(Bucket=bucket_name, ACL=acl)
+
+    client.put_object(Bucket=bucket_name, Key=key, Body=content, ContentType=content_type, CacheControl="no-cache")
 
 
-def _sqs_create_queue(client: BotoBaseClient, name: str) -> Any:
-    queue = client.create_queue(QueueName=name)
-    return queue["QueueUrl"]
+def _kinesis_create_stream(client: BotoBaseClient, stream_name: str) -> Any:
+    client.create_stream(StreamName=stream_name, ShardCount=1)
+    kinesis_waiter = client.get_waiter("stream_exists")
+    while True:
+        try:
+            kinesis_waiter.wait(StreamName=stream_name)
+        except Exception:
+            time.sleep(1)
+        else:
+            break
+
+    return client.describe_stream(StreamName=stream_name)
 
 
-def _sqs_get_messages(client: BotoBaseClient, queue_url: str) -> list[Any]:
+def _kinesis_put_records(client: BotoBaseClient, stream_name: str, records_data: list[str]) -> None:
+    records: list[dict[str, str]] = []
+    for data in records_data:
+        records.append(
+            {
+                "PartitionKey": "PartitionKey",
+                "Data": base64.b64encode(data.encode("utf-8")).decode("utf-8"),
+            }
+        )
+
+    client.put_records(Records=records, StreamName=stream_name)
+
+
+def _kinesis_retrieve_event_from_kinesis_stream(
+    client: BotoBaseClient, stream_name: str, stream_arn: str
+) -> tuple[dict[str, Any], list[int]]:
+    shards_paginator = client.get_paginator("list_shards")
+    shards_available = [
+        shard
+        for shard in shards_paginator.paginate(
+            StreamName=stream_name,
+            ShardFilter={"Type": "FROM_TRIM_HORIZON", "Timestamp": datetime.datetime(2015, 1, 1)},
+            PaginationConfig={"MaxItems": 1, "PageSize": 1},
+        )
+    ]
+
+    assert len(shards_available) == 1 and len(shards_available[0]["Shards"]) == 1
+
+    shard_iterator = client.get_shard_iterator(
+        StreamName=stream_name,
+        ShardId=shards_available[0]["Shards"][0]["ShardId"],
+        ShardIteratorType="TRIM_HORIZON",
+        Timestamp=datetime.datetime(2015, 1, 1),
+    )
+
+    records = client.get_records(ShardIterator=shard_iterator["ShardIterator"])
+
+    assert "Records" in records
+
+    new_records: list[dict[str, Any]] = []
+    collected_records_approximate_arrival_timestamp: list[int] = []
+    for original_record in records["Records"]:
+        kinesis_record = {}
+
+        for key in original_record:
+            new_value = deepcopy(original_record[key])
+            camel_case_key = "".join([key[0].lower(), key[1:]])
+            if isinstance(new_value, bytes):
+                new_value = new_value.decode("utf-8")
+
+            kinesis_record[camel_case_key] = new_value
+
+        kinesis_record["approximateArrivalTimestamp"] = kinesis_record["approximateArrivalTimestamp"].timestamp()
+
+        new_records.append(
+            {
+                "kinesis": kinesis_record,
+                "eventSource": "aws:kinesis",
+                "eventSourceARN": stream_arn,
+            }
+        )
+
+        collected_records_approximate_arrival_timestamp.append(kinesis_record["approximateArrivalTimestamp"])
+
+    return dict(Records=new_records), collected_records_approximate_arrival_timestamp
+
+
+def _sqs_create_queue(client: BotoBaseClient, queue_name: str, endpoint_url: str = "") -> dict[str, Any]:
+    queue_url = client.create_queue(QueueName=queue_name)["QueueUrl"]
+    queue_arn = client.get_queue_attributes(QueueUrl=queue_url, AttributeNames=["QueueArn"])["Attributes"]["QueueArn"]
+    return {
+        "QueueArn": queue_arn,
+        "QueueUrl": queue_url,
+        "QueueUrlPath": queue_url.replace(endpoint_url, f"https://sqs.{_AWS_REGION}.amazonaws.com"),
+    }
+
+
+def _sqs_send_messages(client: BotoBaseClient, queue_url: str, message_body: str) -> None:
+    client.send_message(
+        QueueUrl=queue_url,
+        MessageBody=message_body,
+    )
+
+
+def _sqs_get_messages(client: BotoBaseClient, queue_url: str, queue_arn: str = "") -> tuple[dict[str, Any], list[str]]:
     """
     A function to extract all messages from a SQS queue, specified by URL.
     """
-    collected_messages: list[Any] = []
+    collected_messages: list[dict[str, Union[Any, dict[str, Union[str, Any]]]]] = []
+    sent_timestamps: list[str] = []
     while True:
         try:
-            messages = client.receive_message(QueueUrl=queue_url, MessageAttributeNames=["All"])
+            messages = client.receive_message(QueueUrl=queue_url, AttributeNames=["All"], MessageAttributeNames=["All"])
             # NOTE: asserts are used to check if messages are present.
             # On AssertionError the collected messages are returned.
             assert "Messages" in messages
             assert len(messages["Messages"]) == 1
-            message = messages["Messages"][0]
+            original_message = messages["Messages"][0]
+
+            message: dict[str, Any] = {}
+            for key in original_message:
+                new_value = deepcopy(original_message[key])
+                camel_case_key = "".join([key[0].lower(), key[1:]])
+                message[camel_case_key] = new_value
+
+            if "messageAttributes" in message:
+                for attribute in message["messageAttributes"]:
+                    new_attribute = deepcopy(message["messageAttributes"][attribute])
+                    for attribute_key in message["messageAttributes"][attribute]:
+                        camel_case_key = "".join([attribute_key[0].lower(), attribute_key[1:]])
+                        new_attribute[camel_case_key] = new_attribute[attribute_key]
+                        new_attribute[attribute_key] = ""
+
+                    message["messageAttributes"][attribute] = new_attribute
+
+            sent_timestamps.append(str(int(message["attributes"]["SentTimestamp"])))
+
+            message["eventSource"] = "aws:sqs"
+            message["eventSourceARN"] = queue_arn
+
             collected_messages.append(message)
         except client.exceptions.OverLimit:
             break
         except AssertionError:
             break
 
-    return collected_messages
+    # sqs is not FIFO, let's make it for tests' sake
+    collected_messages.sort(key=lambda x: x["attributes"]["SentTimestamp"])
+
+    return dict(Records=collected_messages), sent_timestamps
 
 
-def _sqs_get_queue_arn(sqs_client: BotoBaseClient, queue_url: str) -> str:
-    return str(
-        sqs_client.get_queue_attributes(QueueUrl=queue_url, AttributeNames=["QueueArn"])["Attributes"]["QueueArn"]
+def _sqs_get_queue_arn(client: BotoBaseClient, queue_url: str) -> str:
+    return str(client.get_queue_attributes(QueueUrl=queue_url, AttributeNames=["QueueArn"])["Attributes"]["QueueArn"])
+
+
+def _create_secrets(client: BotoBaseClient, secret_name: str, secret_data: dict[str, str]) -> Any:
+    client.create_secret(Name=secret_name, SecretString=json_dumper(secret_data))
+    return client.describe_secret(SecretId=secret_name)["ARN"]
+
+
+def _sqs_send_s3_notifications(client: BotoBaseClient, queue_url: str, bucket_name: str, filenames: list[str]) -> None:
+    records = []
+    for filename in filenames:
+        records.append(
+            {
+                "eventVersion": "2.1",
+                "eventSource": "aws:s3",
+                "awsRegion": "eu-central-1",
+                "eventTime": _S3_NOTIFICATION_EVENT_TIME,
+                "eventName": "ObjectCreated:Put",
+                "s3": {
+                    "s3SchemaVersion": "1.0",
+                    "configurationId": "test-bucket",
+                    "bucket": {
+                        "name": bucket_name,
+                        "arn": f"arn:aws:s3:::{bucket_name}",
+                    },
+                    "object": {
+                        "key": f"{filename}",
+                    },
+                },
+            }
+        )
+
+    client.send_message(
+        QueueUrl=queue_url,
+        MessageBody=json_dumper({"Records": records}),
     )
-
-
-def _sqs_patch_messages(messages: list[Any], queueArn: str) -> dict[str, Any]:
-    patched = []
-    for original_message in messages:
-        message: dict[str, Any] = {}
-        for key in original_message:
-            new_value = deepcopy(original_message[key])
-            camel_case_key = "".join([key[0].lower(), key[1:]])
-            message[camel_case_key] = new_value
-
-        if "messageAttributes" in message:
-            for attribute in message["messageAttributes"]:
-                new_attribute = deepcopy(message["messageAttributes"][attribute])
-                for attribute_key in message["messageAttributes"][attribute]:
-                    camel_case_key = "".join([attribute_key[0].lower(), attribute_key[1:]])
-                    new_attribute[camel_case_key] = new_attribute[attribute_key]
-
-                message["messageAttributes"][attribute] = new_attribute
-
-        message["eventSource"] = "aws:sqs"
-        message["eventSourceARN"] = queueArn
-        patched.append(message)
-
-    return dict(Records=patched)
--- tests/testcontainers/logstash.py
+++ tests/testcontainers/logstash.py
@@ -2,14 +2,17 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 
-import json
 import os
+import ssl
 import time
 from typing import Any
 
 import requests
+from OpenSSL import crypto as OpenSSLCrypto
 from testcontainers.core.container import DockerContainer  # type: ignore
 from testcontainers.core.waiting_utils import wait_container_is_ready  # type: ignore
+
+from share import json_parser
 
 
 class LogstashContainer(DockerContainer):  # type: ignore
@@ -23,29 +26,14 @@ class LogstashContainer(DockerContainer):  # type: ignore
         with LogstashContainer() as lc:
             # NOTE: container will terminate once out of this with statement
             url = lc.get_url()
-
-    With custom config
-    ------------------
-    ::
-
-        lgc = LogstashContainer(port=self.logstash_http_port)
-        logstash_config = '''\
-            input {{
-              http {{
-                port => {logstash_http_port}
-                codec => json_lines
-              }}
-            }}
-
-            output {{ stdout {{ codec => json_lines }} }}
-            '''.format(logstash_http_port=self.logstash_http_port)
-        lgc.with_env("CONFIG_STRING", logstash_config)
     """
 
     _DEFAULT_IMAGE = "docker.elastic.co/logstash/logstash"
     _DEFAULT_VERSION = "7.16.0"
     _DEFAULT_PORT = 5044
     _DEFAULT_API_PORT = 9600
+    _DEFAULT_USERNAME = "USERNAME"
+    _DEFAULT_PASSWORD = "PASSWORD"
 
     def __init__(
         self,
@@ -53,98 +41,158 @@ class LogstashContainer(DockerContainer):  # type: ignore
         version: str = _DEFAULT_VERSION,
         port: int = _DEFAULT_PORT,
         api_port: int = _DEFAULT_API_PORT,
+        username: str = _DEFAULT_USERNAME,
+        password: str = _DEFAULT_PASSWORD,
     ):
         image = f"{image}:{version}"
         super(LogstashContainer, self).__init__(image=image)
+        self.with_command("infinity")
 
         self.port = port
         self.api_port = api_port
+        self.exposed_port = 0
+        self.ssl_assert_fingerprint = ""
+
+        self.logstash_user: str = username
+        self.logstash_password: str = password
 
         self.with_exposed_ports(self.port)
         self.with_exposed_ports(self.api_port)
-        self._configure()
 
-        self._previous_message_count = 0
+        self._last_reset_message_count: int = 0
+        self._previous_message_count: int = 0
 
     def _configure(self) -> None:
         """
-        You can override any value set here by calling <instance>.with_env(...) after initializing this class
+        Values set here will override any value set by calling <instance>.with_env(...)
+        after initializing this class before <instance>.start()
         """
-        self.with_env("LOG_LEVEL", os.environ.get("LOGSTASH_LOG_LEVEL", "warn"))
+
+        # TODO: let's generate the certificate and key with code, current cert will expire Nov 17 15:04:09 2025 GMT
+        local_ssl_path = os.path.join(os.path.dirname(__file__), "ssl")
+        self.with_volume_mapping(local_ssl_path, "/ssl")
+
+        self.with_command(
+            'bash -c "/opt/logstash/bin/logstash-plugin install '
+            'logstash-input-elastic_serverless_forwarder && /opt/logstash/bin/logstash"'
+        )
+
+        # NOTE: plain curly brackets must be escaped in this string (double them)
+        logstash_config = f"""\
+            input {{
+              elastic_serverless_forwarder {{
+                port => {self.port}
+                ssl_certificate => "/ssl/localhost.crt"
+                ssl_key => "/ssl/localhost.pkcs8.key"
+                auth_basic_username => "{self.logstash_user}"
+                auth_basic_password => "{self.logstash_password}"
+              }}
+            }}
+            output {{ stdout {{ codec => json_lines }} }}
+            """
+
+        self.with_env("LOG_LEVEL", os.environ.get("LOGSTASH_LOG_LEVEL", "fatal"))
         self.with_env("PIPELINE_WORKERS", "1")
         # By design and by default, Logstash does not guarantee event order. The following setting ensures that
         # events are processed in the order they are received.
         # Ref: https://www.elastic.co/guide/en/logstash/current/processing.html
         self.with_env("PIPELINE_ORDERED", "true")
-        self.with_env("CONFIG_STRING", "input { stdin {} } output { stdout {} }")
-        self.with_env("XPACK_MONITORING_ENABLED", "false")
+        self.with_env("MONITORING_ENABLED", "false")
+        self.with_env("CONFIG_STRING", logstash_config)
 
-    def get_url(self, ssl: bool = False) -> str:
+    def get_url(self) -> str:
         host = self.get_container_host_ip()
         port = self.get_exposed_port(self.port)
-        if ssl:
-            http_protocol = "https"
-        else:
-            http_protocol = "http"
-        return f"{http_protocol}://{host}:{port}"
+        return f"https://{host}:{port}"
 
-    def get_apiurl(self) -> str:
+    def get_api_url(self) -> str:
         host = self.get_container_host_ip()
         port = self.get_exposed_port(self.api_port)
         return f"http://{host}:{port}"
 
     @wait_container_is_ready(requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout)  # type: ignore
     def _connect(self) -> None:
-        url = self.get_apiurl()
+        self.host = self.get_container_host_ip()
+        self.exposed_port = int(self.get_exposed_port(self.port))
+
+        while True:
+            try:
+                pem_server_certificate: str = ssl.get_server_certificate((self.host, self.exposed_port))
+                openssl_certificate = OpenSSLCrypto.load_certificate(
+                    OpenSSLCrypto.FILETYPE_PEM, pem_server_certificate.encode("utf-8")
+                )
+            except Exception:
+                time.sleep(1)
+            else:
+                self.ssl_assert_fingerprint = str(openssl_certificate.digest("sha256").decode())
+                break
+
+        assert len(self.ssl_assert_fingerprint) > 0
+
+        url = self.get_api_url()
         response = requests.get(f"{url}/?pretty", timeout=1)
         response.raise_for_status()
 
+    def reset(self) -> None:
+        self._last_reset_message_count = self._previous_message_count
+
     def start(self) -> "LogstashContainer":
+        self._configure()
         super().start()
         self._connect()
         return self
 
-    def get_messages(self, retry: int = 2, timeout: int = 1, delay: int = 1) -> list[dict[str, Any]]:
+    def get_messages(self, expected: int, retry: int = 2, wait_time: int = 1, delay: int = 1) -> list[dict[str, Any]]:
         """
-        Extract JSON messages from stdout as printed by Logstash with codec json or json_lines.
+        Extract N expected JSON messages from stdout as printed by Logstash with codec json or json_lines.
 
         NOTE: to achieve the desired result the Logstash configuration should output
         JSON to stdout using stdout plugin with codec json or json_lines.
 
-        NOTE: a delay has been observed between data beign sent to Logstash and it
+        NOTE: a delay has been observed between data being sent to Logstash and it
         being available in the Docker stdout. To accout for this delay without
         the need to add sleeps in other areas this function has a retry logic (enabled
         by default) implemented. After an initial delay a retry mechanism is triggered
         based on number of messages expected in the docker output, with an adjustable
         timeout between retries.
-
+        :param expected: The number of messages to fetch.
+            It accepts a 0 value.
         :param retry: The number of times to retry fetch messages.
             To disable set it to 0.
-        :param timeout: The timeout in seconds to wait between each retry.
+        :param wait_time: The time in seconds to wait between each retry.
             To disable set it to 0.
         :param delay: The delay to start gathering logs from Docker daemon.
             To disable set it to 0.
         :return: The list of parsed messages.
         """
+
         time.sleep(delay)
-        stdout, stderr = self.get_logs()
-        lines = stdout.decode().split("\n")
+        stdout: bytes = b""
+        stdout, _ = self.get_logs()
         messages = []
+        lines_count: int = stdout.count(b"\n")
+        if lines_count < expected:
+            if wait_time > 0:
+                time.sleep(wait_time)
+            return self.get_messages(expected=expected, retry=retry, wait_time=wait_time, delay=0)
+
+        lines = stdout.decode("utf-8").splitlines()
         for line in lines:
             try:
-                msg = json.loads(line)
+                msg = json_parser(line)
                 messages.append(msg)
-            except json.decoder.JSONDecodeError:
+            except Exception:
                 # NOTE: if a line is not valid JSON is not a message sent to stdout,
                 # so we can safely ignore it
                 pass
 
         # Using the previous message count allows subsequent calls to this function
         # to properly trigger replay logic.
-        if len(messages) == self._previous_message_count and retry > 0:
-            if timeout > 0:
-                time.sleep(timeout)
-            return self.get_messages(retry - 1, timeout)
+        if len(messages) < expected and retry > 0:
+            if wait_time > 0:
+                time.sleep(wait_time)
+            return self.get_messages(expected=expected, retry=retry - 1, wait_time=wait_time, delay=0)
 
         self._previous_message_count = len(messages)
-        return messages
+
+        return messages[self._last_reset_message_count :]
