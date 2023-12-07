67a7defb7e968df0e702908fd94802050a478700
Andrea Spacca <andrea.spacca@elastic.co>
2023-12-07 11:59:25 +0900

    disbale gzip compression in ES client

--- shippers/es.py
+++ shippers/es.py
@@ -97,7 +97,6 @@ class ElasticsearchShipper:
             es_client_kwargs["ssl_assert_fingerprint"] = ssl_assert_fingerprint
 
         es_client_kwargs["serializer"] = JSONSerializer()
-        es_client_kwargs["http_compress"] = False
 
         self._replay_args: dict[str, Any] = {}
 
@@ -121,7 +120,7 @@ class ElasticsearchShipper:
 
         es_client_kwargs["timeout"] = 30
         es_client_kwargs["max_retries"] = 4
-        es_client_kwargs["http_compress"] = True
+        es_client_kwargs["http_compress"] = False
         es_client_kwargs["retry_on_timeout"] = True
         es_client_kwargs["headers"] = {
             "User-Agent": share.utils.create_user_agent(esf_version=version, environment=get_environment())
