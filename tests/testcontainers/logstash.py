b83c882785029ad362bf7e2e4400d8a298f0e834
constanca <constanca.manteigas@elastic.co>
2024-04-17 15:17:38 +0200

    .

--- tests/testcontainers/logstash.py
+++ tests/testcontainers/logstash.py
@@ -201,8 +201,8 @@ EOF
         self._last_reset_message_count = self._previous_message_count
 
     def start(self) -> LogstashContainer:
-        super().start()
         self._configure()
+        super().start()
         self._connect()
         return self
 
